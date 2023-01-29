218b04f9d24490c6746e1943a365014074cb9272
dsborisovv <ds.borisov@yandex.ru>
2023-01-30 02:05:24 +0300

    Final asyncio parser

--- app/parser_asyncio.py
+++ app/parser_asyncio.py
@@ -1,78 +1,76 @@
-import json
+import sys
 import time
+import csv
 
-import requests
 from bs4 import BeautifulSoup
 import fake_useragent
 import asyncio
 import aiohttp
 
-vac = []
-def iterator(soup):
+jobs = []
+
+
+def link_iterator(soup):
     for a in soup.find_all("a", attrs={"class": "serp-item__title"}):
         yield f"{a.attrs['href'].split('?')[0]}"
 
 
-async def get_page_data(session, iterator1):
+async def get_page_data(session, iterator_page):
     ua = fake_useragent.UserAgent()
-    for url in iterator1:
+    for url in iterator_page:
         async with session.get(url=url, headers={
                     "user-agent": ua.random
                 }) as response:
             response_text = await response.text()
-            soup1 = BeautifulSoup(response_text, "lxml")
-            title = soup1.find(attrs={"data-qa": "vacancy-title"}).text
-            experience = soup1.find(attrs={"data-qa": "vacancy-experience"}).text
-            salary = soup1.find(
+            soup = BeautifulSoup(response_text, "lxml")
+            title = soup.find(attrs={"data-qa": "vacancy-title"}).text
+            experience = soup.find(attrs={"data-qa": "vacancy-experience"}).text
+            salary = soup.find(
                 attrs={
                     "class": "bloko-header-section-2 bloko-header-section-2_lite"
                 }
             ).text.replace("\xa0", "")
-            employment = soup1.find(
+            employment = soup.find(
                 attrs={"data-qa": "vacancy-view-employment-mode"}
             ).text
-            company = soup1.find(
+            company = soup.find(
                 attrs={"data-qa": "bloko-header-2"}
             ).text.replace("\xa0", " ")
             try:
-                description = soup1.find(attrs={"class": "g-user-content"}).text
+                description = soup.find(attrs={"class": "vacancy-branded-user-content"}).text
             except:
-                description = ""
+                description = soup.find(attrs={"class": "g-user-content"}).text
             try:
-                tags = [tag.text.replace("\xa0", " ") for tag in soup1.find(
+                tags = [tag.text.replace("\xa0", " ") for tag in soup.find(
                     attrs={"class": "bloko-tag-list"}
                 ).find_all(attrs={"class": "bloko-tag__section_text"})]
             except:
                 tags = []
 
-            vac.append({
-                'title': title,
-                'link': url,
-                'experience': experience,
-                'salary': salary,
-                'employment': employment,
-                'company': company,
-                'description': description,
-                "tags": tags
-            })
+            jobs.append(
+                [
+                    title,
+                    url,
+                    experience,
+                    salary,
+                    employment,
+                    company,
+                    description,
+                    tags
+                ]
+            )
 
 
-
-
-
-
-
-
-async def get_count_pages(text):
+async def get_tasks(text):
     ua = fake_useragent.UserAgent()
-    url = f"https://hh.ru/search/vacancy?area=113&search_field=name&search_field=company_name&search_field=description&text={text}&from=suggest_post&page=1"
+    url_base = f"https://hh.ru/search/vacancy?area=113&search_field=name&search_field=company_name&search_field=description&text={text}&from=suggest_post&page=1"
     async with aiohttp.ClientSession() as session:
-        response = await session.get(url=url, headers={
+        response_base = await session.get(url=url_base, headers={
                 "user-agent": ua.random
             })
-        soup = BeautifulSoup(await response.text(), "lxml")
+        soup_base = BeautifulSoup(await response_base.text(), "lxml")
         page_count = int(
-            soup.find(
+            soup_base.find(
                 "div",
                 attrs={
                     "class": "pager"
@@ -85,22 +83,32 @@ async def get_count_pages(text):
 
         tasks = []
 
-        for page in range(1):
-            url1 = f"https://hh.ru/search/vacancy?area=113&search_field=name&search_field=company_name&search_field=description&text={text}&from=suggest_post&page={page}"
-            response1 = await session.get(url=url1, headers={
-                "user-agent": ua.random
-            })
-            soup1 = BeautifulSoup(await response1.text(), "lxml")
-            iterator1 = iterator(soup1)
-            task = asyncio.create_task(get_page_data(session, iterator1))
+        for page in range(5):
+            url_page = f"https://hh.ru/search/vacancy?area=113&search_field=name&search_field=company_name&search_field=description&text={text}&from=suggest_post&page={page}"
+            response_page = await session.get(
+                url=url_page, headers={
+                    "user-agent": ua.random
+                }
+            )
+            soup_page = BeautifulSoup(await response_page.text(), "lxml")
+            iterator_page = link_iterator(soup_page)
+            task = asyncio.create_task(get_page_data(session, iterator_page))
             tasks.append(task)
+            await asyncio.sleep(1)
         await asyncio.gather(*tasks)
 
-async def tasks():
-    await get_count_pages("python")
+
+async def _tasks(vac_name):
+    await get_tasks(vac_name)
+
+
 if __name__ == "__main__":
+    print("Start parsing...")
     start_time = time.time()
-    asyncio.get_event_loop().run_until_complete(tasks())
-    # print(vac)
-    print('All good!')
-    print(f'Elapsed time {time.time() - start_time}')
+    vacancy_name = ' '.join(sys.argv[1:])
+    asyncio.get_event_loop().run_until_complete(_tasks(vacancy_name))
+    with open(r"data\data.csv", "a", encoding="utf-8", errors="ignore") as file:
+        writer = csv.writer(file)
+        for row in jobs:
+            writer.writerow(row)
+    print(f'Elapsed time: {round(time.time() - start_time, 2)}')
